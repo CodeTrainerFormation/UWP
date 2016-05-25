1b1d7d1c982f058211ab9e5973d5591f0ad2553a
Azunyth <romain.rukse@codetrainer.fr>
2016-05-25 22:23:02 +0200

    Ajout début exercice 6-Tile

---
+++ Corrections/6-Tile/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _6_Tile
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void EditTile_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.Data.Xml.Dom.XmlDocument doc = new Windows.Data.Xml.Dom.XmlDocument();
+            string xml = File.ReadAllText("Templates/Tile.xml");
+            doc.LoadXml(xml);
+
+            var tile = new TileNotification(doc);
+
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tile);
+        }
+
+        private void CleanTile_Click(object sender, RoutedEventArgs e)
+        {
+            TileUpdateManager.CreateTileUpdaterForApplication().StopPeriodicUpdate();
+            TileUpdateManager.CreateTileUpdaterForApplication().Clear();
+        }
+    }
+}
